f48d1959ce92c4895506c0715ec922f2d59af0f5
Polar Rex <30412580+polar-rex@users.noreply.github.com>
2018-02-08 21:19:28 +0800

    2018-02-08 Update

--- cogs/general.py
+++ cogs/general.py
@@ -3,6 +3,8 @@ import aiohttp
 import asyncio
 import bs4
 import discord
+import datetime
+import pytz
 from discord.ext import commands
 
 
@@ -15,6 +17,14 @@ class General:
         return await r.connect("localhost", 28015)
 
         
+    async def convert_datetime(self, tbc):
+        old_timezone = pytz.timezone("UTC")
+        new_timezone = pytz.timezone("Asia/Kuala_Lumpur")
+        present = old_timezone.localize(tbc).astimezone(new_timezone)
+        text = present.strftime('%A %Y-%m-%d %H:%M:%S (UTC+8)')
+        return text
+     
+     
     @commands.group(invoke_without_command=True)
     async def help(self, ctx, arg='help'):
         slate = discord.Embed(colour=7506394)
@@ -101,17 +111,106 @@ class General:
         
         
     @commands.command()
-    async def source(self, ctx):
-        await ctx.send("https://github.com/polar-rex/Polar-Bot
-        
-        
-    @commands.command()
     async def info(self, ctx, person:discord.Member=None):
         if person == None:
             person = ctx.author
-        avatar
-        await ctx.send(avatar)
+        slate = discord.Embed(title='{} (Displayed as {})'.format(str(person), person.display_name), colour=7506394)
         
+        if person.game == None:
+            game = 'Nothing'
+        
+        else:
+            game = person.game.name
+            
+        roles = ''
+        for y in [x.name for x in person.roles][1:]:
+            roles += y + '\n'
+      
+        if person.voice == None:
+            voice = 'Not Connected'
+            
+        else:
+            voice = 'Connected to {}'.format(person.voice.channel.name)
+        
+        discord_join_date = await self.convert_datetime(person.created_at)
+        server_join_date = await self.convert_datetime(person.joined_at)
+        
+        fields = [
+            ('STATUS', str(person.status), True),
+            ('PLAYING', game, True),
+            ('VOICE STATUS', voice, True),
+            ('AVATAR URL', '[Link]({})'.format(person.avatar_url), True),
+            ('DISCORD JOIN DATE', discord_join_date, False),
+            ('SERVER JOIN DATE', server_join_date, False),
+            ('ROLES', roles, False),
+        ]
+        
+        for x in fields:
+            slate.add_field(name=x[0], value=x[1], inline=x[2])
+        
+        slate.set_thumbnail(url=person.avatar_url)
+        slate.set_footer(text='USERNAME ID: {}'.format(person.id))
+            
+        await ctx.send(embed=slate)
+
+        
+    @commands.command()
+    async def server(self, ctx):
+        guild = ctx.guild
+        emoji_text = '**EMOJI**\n' + ''.join([str(emoji) for emoji in guild.emojis])
+        slate = discord.Embed(title=guild.name, description=emoji_text, colour=7506394)
+        
+        creation_date = await self.convert_datetime(guild.created_at)
+        afk_text = str(int(guild.afk_timeout / 60)) + ' minutes'
+        
+        fields = [
+            ('MEMBER COUNT', str(len(guild.members)), True),
+            ('CREATED ON', creation_date[9:-8], True),
+            ('OWNER', '<@{}>'.format(guild.owner.id), True),
+            ('AFK TIMEOUT', afk_text, True),
+            ('VOICE REGION', str(guild.region), True),
+            ('ICON URL', '[Link]({})'.format(guild.icon_url), True),
+            ('INVITE LINK', 'https://discord.gg/gCpJ9BF', False),
+        ]
+        
+        for z in fields:
+            slate.add_field(name=z[0], value=z[1], inline=z[2])
+            
+        slate.set_thumbnail(url=guild.icon_url)
+        slate.set_footer(text='GUILD ID: {}'.format(guild.id))
+        
+        await ctx.send(embed=slate)
+        
+        
+    @commands.command()
+    async def about(self, ctx):
+        user = self.bot.user
+        creation_date = await self.convert_datetime(user.created_at)
+        
+        desc = 'Hi! I am a bot created and maintained by <@{}> for the /r/Malaysia Discord server.\n\n'.format(266107973128945664)
+        
+        desc += '**BACKGROUND**\n'
+        desc += '`Username      : {}`\n'.format(str(user))
+        desc += '`Username ID   : {}`\n'.format(user.id)
+        desc += '`Creation Date : {}`\n\n'.format(creation_date)
+        
+        desc += '**DEVELOPMENT\n**'
+        desc += '`Language      : Python 3.6`\n'
+        desc += '`Library       : discord.py 1.0.0a0`\n\n'
+        
+        slate = discord.Embed(description=desc, colour=7506394)
+        
+        fields = [
+            ('AVATAR URL', '[Link]({})'.format(user.avatar_url), True),
+            ('SOURCE CODE', '[Link](https://github.com/polar-rex/Polar-Bot)', True),
+        ]
+        
+        for x in fields:
+            slate.add_field(name=x[0], value=x[1], inline=x[2])
+        
+        slate.set_thumbnail(url=user.avatar_url)
+            
+        await ctx.send(embed=slate)
         
     @commands.command()
     async def yt(self, ctx, *, search:str=None):
@@ -131,7 +230,7 @@ class General:
                     await asyncio.sleep(0)
                     
                 await abstr.postpages(self.bot, ctx, links)
-        
+               
         
 def setup(bot):
     bot.add_cog(General(bot))
